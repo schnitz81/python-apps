f7b125eef1f308d4603a5b353e20c1fac7067bc1
schnitz81 <k.quick81@gmail.com>
2021-08-03 21:51:55 +0000

    3 part colorwave

--- ttycolorwave/ttycolorwave.py
+++ ttycolorwave/ttycolorwave.py
@@ -21,26 +21,45 @@ def switch_back_color(rndnbr):
 
 
 def waver():
-    pos = 39
+    pos1 = 26
+    pos2 = 52
+
     while True:
         col1 = random.randint(1, 7)
         col2 = random.randint(1, 7)
-        while col2 == col1:
+        col3 = random.randint(1, 7)
+        while col1 == col2:
             col2 = random.randint(1, 7)
-        for x in range(1, 100):
+        while col3 == col1 or col3 == col2:
+            col3 = random.randint(1, 7)
+        for x in range(1, 200):
             switch_back_color(col1)
-            for i in range(0, pos):
+
+            for i in range(0, pos1):
                 print(' ', end="")
             switch_back_color(col2)
-            for i in range(0, 80 - pos):
+            for i in range(0, pos2 - pos1):
+                print(' ', end="")
+            switch_back_color(col3)
+            for i in range(0, 80 - pos2):
                 print(' ', end="")
             print(Back.RESET)  # newline
+
+            # pos1 movement
             rnd = random.randint(0, 2)
-            if rnd == 0 and pos < 80 or pos <= 0:
-                pos += 1
-            elif rnd == 1 or pos >= 80:
-                pos -= 1
-            time.sleep(.03)
+            if rnd == 0 and pos1 < 80 and pos1 < pos2 or pos1 <= 0:
+                pos1 += 1
+            elif rnd == 1 or pos1 >= 80:
+                pos1 -= 1
+
+            # pos2 movement
+            rnd = random.randint(0, 2)
+            if rnd == 0 and pos2 < 80 or pos2 <= 0:
+                pos2 += 1
+            elif rnd == 1 and pos2 > pos1 or pos2 >= 80:
+                pos2 -= 1
+
+            time.sleep(.02)
 
 
 if __name__ == '__main__':
